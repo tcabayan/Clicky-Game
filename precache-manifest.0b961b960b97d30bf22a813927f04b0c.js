a1ad670c3be18ad3a6c1da3cfb3b5c0ba9b5f69e
Tamar Cabayan <tcabayan@gmail.com>
2020-06-23 17:33:32 -0700

    Updates

---
+++ precache-manifest.0b961b960b97d30bf22a813927f04b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b53e2ac38ebbd7040358b7f286adca",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "f42cbadbf4da734ac94f",
+    "url": "/Clicky-Game/static/css/main.aaedf9d3.chunk.css"
+  },
+  {
+    "revision": "c7c542db1198810108d0",
+    "url": "/Clicky-Game/static/js/2.73cfe274.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.73cfe274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42cbadbf4da734ac94f",
+    "url": "/Clicky-Game/static/js/main.e635ec36.chunk.js"
+  },
+  {
+    "revision": "a7a26ccd82966f08611f",
+    "url": "/Clicky-Game/static/js/runtime-main.e9c17cb4.js"
+  }
+]);
